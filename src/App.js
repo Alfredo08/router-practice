dca707c1a885d17452be686977eadbf2ff1c756d
Alfredo Salazar <>
2019-12-30 11:57:24 -0600

    Added context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 import Profile from './Profile';
+import AppContext from './AppContext';
 
 class App extends React.Component {
 
@@ -28,14 +29,21 @@ class App extends React.Component {
       ]
     }
   }
-
+  
   render(){
+
+    const contextValues = {
+      profiles : this.state.profiles
+    };
+
     return (
       <BrowserRouter>
-        <Link to="/"> Home </Link>
-        <Link to="/profile"> Profile </Link>
-        {/* <Route path="/profile" component={Profile} /> */}
-        <Route path="/profile/:identifier&:id" render={ (props) => <Profile {...props} profiles={this.state.profiles}/> } />
+        <AppContext.Provider value={contextValues}>
+          <Link to="/"> Home </Link>
+          <Link to="/profile"> Profile </Link>
+          <Route path="/profile/:identifier" component={Profile} />
+          {/* <Route path="/profile/:identifier&:id" render={ (props) => <Profile {...props} profiles={this.state.profiles}/> } /> */}
+        </AppContext.Provider>
       </BrowserRouter>
     );
   }
--- src/Profile.js
+++ src/Profile.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import AppContext from './AppContext';
 
 export default function Profile(props){
 
-    console.log( props );
-    let identifier = props.match.params.identifier;
-
-    let currentProfile = props.profiles.find( profile => {
-        console.log( profile.id, identifier );
-        return identifier == profile.id;
-    });
     return(
-        <div>
-            <h1> This is the profile of {currentProfile.firstName} {currentProfile.lastName} </h1>
-            <p> At Thinkful he is a {currentProfile.role} </p>
-            <div>
-                <img src={currentProfile.image} alt="An image" />
-            </div>
-            <h2> His bio</h2>
-            <p> {currentProfile.biography} </p>
-        </div>
+        <AppContext.Consumer>
+            {
+                function contextStuff(context){
+                    console.log( context );
+                    let identifier = props.match.params.identifier;
+
+                    let currentProfile = context.profiles.find( profile => {
+                        console.log( profile.id, identifier );
+                        return identifier == profile.id;
+                    });
+
+                    return (
+                        <div>
+                            <h1> This is the profile of {currentProfile.firstName} {currentProfile.lastName} </h1>
+                            <p> At Thinkful he is a {currentProfile.role} </p>
+                            <div>
+                                <img src={currentProfile.image} alt="An image" />
+                            </div>
+                            <h2> His bio</h2>
+                            <p> {currentProfile.biography} </p>
+                        </div>
+                    )
+                }
+            }
+        </AppContext.Consumer>
     );
+
 }
