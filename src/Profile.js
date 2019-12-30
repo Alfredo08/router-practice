05521ed7747363fececb23d8a1c30745c255d0b8
Alfredo Salazar <>
2019-12-30 12:06:51 -0600

    Added context with class component

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import AppContext from './AppContext';
 
+
+/* Importing context with a class compoenent 
+export default class Profile extends React.Component{
+
+    static contextType = AppContext;
+
+    render (){
+        console.log( this.context );
+        let identifier = this.props.match.params.identifier;
+
+        let currentProfile = this.context.profiles.find( profile => {
+            console.log( profile.id, identifier );
+            return identifier == profile.id;
+        });
+        return(
+            <div>
+                <h1> This is the profile of {currentProfile.firstName} {currentProfile.lastName} </h1>
+                <p> At Thinkful he is a {currentProfile.role} </p>
+                <div>
+                    <img src={currentProfile.image} alt="An image" />
+                </div>
+                <h2> His bio</h2>
+                <p> {currentProfile.biography} </p>
+            </div>
+        );
+    }
+}
+*/
+
+/* Importing context with a functional component */
 export default function Profile(props){
 
     return(
